596ff9904a81b9faa75874edbbbc81479f8f58d5
Richard Odhiambo Otieno <kotienor1@gmail.com>
2024-04-08 18:31:49 +0300

    Events-Listening-Lab

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+// This function adds an event listener to the button element with the ID 'button'
 function addingEventListener() {
+    // get the button element by its ID 
+    let eventTarget = document.getElementById('button');
+    eventTarget.addEventListener('click', function() {
+        alert('You clicked the button!');
+    });
 }
+
+addingEventListener();
